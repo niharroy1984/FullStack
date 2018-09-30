1200a8e6fa7be9714d2a02f185635516ded6cca4
niharroy1984 <43709130+niharroy1984@users.noreply.github.com>
2018-09-30 11:46:49 +0530

    Add files via upload

---
+++ Angular-session1.js
@@ -0,0 +1,11 @@
+var Demo = /** @class */ (function () {
+    function Demo() {
+    }
+    Demo.DoDemo = function () {
+        console.log("Welcometo Java");
+        return 1;
+    };
+    return Demo;
+}());
+Demo.DoDemo();
+console.log(Demo.DoDemo());
---
+++ Angular-session1.ts
@@ -0,0 +1,9 @@
+class Demo{
+    public static DoDemo():number{
+        console.log("Welcometo Java")
+        return 1;
+    }
+}
+
+Demo.DoDemo();
+console.log(Demo.DoDemo())
